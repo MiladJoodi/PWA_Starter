f81e90f34a37996f8d25a1f76e3362e6a23bacb5
Joodi <winbeta.com@gmail.com>
2025-07-26 20:34:29 +0330

    up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: isDev,
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
+
